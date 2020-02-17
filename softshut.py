4f9f7f70f6c77a92a12ac931d2982710a03c41dc
Chris Frauenberger <christopher@frauenberger.name>
2020-02-17 16:55:21 +0100

    call set_np.py rather than doing it ourselfs (not run by python3)

--- softshut.py
+++ softshut.py
@@ -4,16 +4,6 @@
 from subprocess import call
 import RPi.GPIO as GPIO
 from time import sleep
-import board
-import neopixel
-
-#Switch off the neopixels
-pixel_pin = board.D18
-num_pixels = 30
-ORDER = neopixel.GRBW
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.6, auto_write=False,
-                           pixel_order=ORDER)
-
 
 # Map pin seven and eight on the Pi Switch PCB to chosen pins on the Raspberry Pi header
 # The PCB numbering is a legacy with the original design of the board
@@ -27,9 +17,11 @@ while (GPIO.input(PinSeven) == False): # While button not pressed
  GPIO.wait_for_edge(PinSeven, GPIO.RISING) # Wait for a rising edge on PinSeven
  sleep(0.1); # Sleep 100ms to avoid triggering a shutdown when a spike occured
 
-pixels.fill((0, 0, 0, 0))
-pixels.show()
+#switch off the neopixels
+call('sudo python3 /home/pi/src/spt-raspberr-cam/set_np.py clear', shell== False)
+
 sleep(2); # Sleep 2s to distinguish a long press from a short press
+
 
 if (GPIO.input(PinSeven) == False):
  GPIO.output(PinEight,0) # Bring down PinEight so that the capacitor can discharge and remove power to the Pi
